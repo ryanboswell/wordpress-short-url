1ee154f4386d4f1ae87906cf2b2c6f6a3d48b34a
Ryan Boswell <ryan@ryanboswell.com>
2013-09-04 08:04:19 -0700

    Initial Commit, add plugin documentation

---
+++ rb-custom-short-url/rb-custom-short-url.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Custom Short URL
+Plugin URI: http://ryanboswell.com
+Description: Provides a simple management page for setting a custom short URL to use when generating shortlinks
+Version: 1.0
+Author: Ryan Boswell
+Author URI: http://ryanboswell.com
+*/
+
+
+?>
